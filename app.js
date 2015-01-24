5c65e11963b83184539ef0ed0f46a7d596fbaa1f
Christian Di Lorenzo <rcdilorenzo@me.com>
2015-01-24 17:17:40 -0500

    Add progress bar

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 app = angular.module('repeatex', []);
 
+app.config(function($httpProvider) {
+  $httpProvider.interceptors.push(function($q) {
+    return {
+      'request': function(config) {
+        NProgress.start();
+        return config;
+      },
+      'requestError': function(rejection) {
+        NProgress.done();
+        return $q.reject(rejection);
+      },
+      'response': function(response) {
+        NProgress.done();
+        return response;
+      },
+      'responseError': function(rejection) {
+        NProgress.done();
+        return $q.reject(rejection);
+      }
+    };
+  });
+});
+
 app.controller('MainController', function($scope, $http) {
   $scope.updateOutput = function() {
     $http.get('http://repeatex.herokuapp.com/?description=' + $scope.description).then(function (response) {
       $scope.output = response.data;
     });
     $scope.issueTitle = 'Parse Issue: "' + $scope.description + '"';
-    $scope.issueBody = "The description \"" + $scope.description + "\" was supposed to be parsed properly, but wasn't. Thanks for your assistance with working on this issue.";
+    $scope.issueBody = "The description \"" + $scope.description + "\" was supposed to be parsed properly, but wasn't.";
   };
 
   $scope.goToDemo = function () {
